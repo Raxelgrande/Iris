1c8ae8555398ac19c327a30ba0f1071611690c55
Space Banana <tomasbessa@protonmail.com>
2024-02-16 21:03:43 +0000

    build script fix

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
 echo "Building lightweight JAR"
-scalac src/*.scala src/*/*.scala src/*/*/*.scala -d iris.jar
+scalac src/*.scala src/*/*.scala -d iris.jar
 echo "Building Fat JAR"
 steam-run scala-cli --power package src --assembly --preamble=false --jvm 8 -o iris-java.jar
