3b4f3fb0f2f6d35cb8ab1b15104b93e2107f4ed1
raxelgrande <lucasvbm2@gmail.com>
2024-02-18 20:43:40 -0500

    debugging distroFinder

--- src/main.scala
+++ src/main.scala
@@ -1,2 +1,6 @@
+import iris.distroFinder.getPackageManager 
+
 @main def main() =
+    println(getPackageManager())    
     //if you read this you are a certified scalamancer
+    //change of plans, we got the bugs, pest control weewooweewoo 
