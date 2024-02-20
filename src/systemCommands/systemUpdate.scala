800060511747887dbf4ceae022e7ace59203e91c
Space Banana <tomasbessa@protonmail.com>
2024-02-20 18:43:56 +0000

    Improvements to systemupdate

--- src/systemCommands/systemUpdate.scala
+++ src/systemCommands/systemUpdate.scala
@@ -1,34 +1,32 @@
 package iris.sysUpdate
-import iris.distroFinder
- import iris.distroFinder.getPackageManager
- import scala.sys.process._
 
- def sysUpdate() = 
-    val getDistro = getPackageManager().mkString
-    if getDistro.contains("pacman") then 
-    pacUpdate()
-    else if getDistro.contains("apt") then
-   aptUpdate() 
-    else if getDistro.contains("dnf") then
-      dnfUpdate()
-    else if getDistro.contains("zypper") then
-      zypperUpdate()
-    else if getDistro.contains("nix") then
-      nixUpdate()
+import iris.distroFinder.getPackageManager
+import scala.sys.process._
+
+def sysUpdate() = 
+  getPackageManager() match
+    case "pacman" => pacUpdate()
+    case "apt" => aptUpdate()
+    case "dnf" => dnfUpdate()
+    case "zypper" => zypperUpdate()
+    case "nix" => nixUpdate()
+    case _ => unknownSystem()
    
-
     
- def pacUpdate() = List("sudo", "pacman", "-Syu", "--noconfirm").run
+def pacUpdate() = List("sudo", "pacman", "-Syu", "--noconfirm").run
     
 def aptUpdate() = 
-   List("sudo", "apt", "update", "-y").run
-   List("sudo", "apt", "upgrade", "-y").run
+  List("sudo", "apt", "update", "-y").run
+  List("sudo", "apt", "upgrade", "-y").run
 
- 
 def dnfUpdate() = List("sudo", "dnf", "upgrade", "-y").run
 
 def zypperUpdate() = List("sudo", "zypper", "dup", "-y").run
 
 def nixUpdate() = List("sudo", "nixos-rebuild", "switch", "--upgrade").run
 
-    
+def unknownSystem() = //remember to change the text based on what is supposed for the person to do next
+  pressToContinue(
+    "Iris could not recognise your system or the package manager it uses"
+    + "\nPlease update your system before installing the required packages"
+    )
