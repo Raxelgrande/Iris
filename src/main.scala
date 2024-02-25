12a24b8546b1ba4618f026079f75fc9056fcb2ee
Raxelgrande <lucasvbm2@gmail.com>
2024-02-25 17:24:59 -0500

    theming.scala and theme checking and setting functions

--- src/main.scala
+++ src/main.scala
@@ -2,7 +2,8 @@ import iris.distroFinder.getPackageManager
 import iris.sysUpdate._
 import iris.dependencyInstall._
 import iris.firstRun._
+import iris.theming._
 
 @main def main() =
-    firstRun()
+    println(gnomeCheckShell())
     //if you read this you are a certified scalamancer
---
+++ src/systemCommands/theming.scala
@@ -0,0 +1,30 @@
+package iris.theming
+import scala.sys.process._
+
+// GNOME & Budgie theme checking
+def gnBgCheckGtk() = List("gsettings", "get", "org.gnome.desktop.interface", "gtk-theme").!!
+def gnBgCheckIcon() = List("gsettings", "get", "org.gnome.desktop.interface", "icon-theme").!!
+def gnBgCheckCursor() = List("gsettings", "get", "org.gnome.desktop.interface", "cursor-theme").!!
+// GNOME Shell theme check (requires user-themes extension)
+def gnomeCheckShell() = List("dconf", "read", "/org/gnome/shell/extensions/user-theme/name").!!
+
+// GNOME & Budgie set a theme
+def gnBgSetGtk() = List("gsettings", "set", "org.gnome.desktop.interface", "gtk-theme", selGtkTheme()).!!
+def gnBgSetIcon() = List("gsettings", "set", "org.gnome.desktop.interface", "icon-theme", selIconTheme()).!!
+def gnBgSetCursor() = List("gsettings", "set", "org.gnome.desktop.interface", "cursor-theme", selCursorTheme()).!!
+// GNOME Shell set a theme 
+def gnomeSetShell() = List("dconf", "write", "/org/gnome/shell/extensions/user-theme/name", s"'${selGnomeTheme()}'").!!
+
+
+// Cinnamon theme checking 
+def cinnamonCheckGtk() = List("gsettings", "get", "org.cinnamon.desktop.interface", "gtk-theme").!!
+def cinnamonCheckIcon() = List("gsettings", "get", "org.cinnamon.desktop.interface", "icon-theme").!!
+def cinnamonCheckCursor() = List("gsettings", "get", "org.cinnamon.desktop.interface", "cursor-theme").!!
+def cinnamonCheckShell() = List("gsetitngs", "get", "org.cinnamon.theme", "name").!!
+
+// Cinnamon set a theme
+def cinnamonSetGtk() = List("gsetitngs", "set", "org.cinnamon.desktop.interface", "gtk-theme", selGtkTheme()).!! 
+def cinnamonSetIcon() = List("gsetitngs", "set", "org.cinnamon.desktop.interface", "icon-theme", selIconTheme()).!!
+def cinnamonSetCursor() = List("gsetitngs", "set", "org.cinnamon.desktop.interface", "cursor-theme", selCursorTheme()).!!
+def cinnamonSetShell() = List("gsetitngs", "set", "org.cinnamon.theme", "name", selCinnamonTheme()).!!
+
