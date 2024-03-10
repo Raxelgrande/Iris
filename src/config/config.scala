0d920bc0a3a3afbe1dcd132fd8e098fdf9722338
Raxelgrande <lucasvbm2@gmail.com>
2024-03-09 21:23:59 -0500

    Fixed formatting 00

--- src/config/config.scala
+++ src/config/config.scala
@@ -9,9 +9,9 @@ def listOfConfigs(): List[String] =
   val irisConfLoc = getHome()+"/.config/Iris/"
   
   val irisConfList = File(irisConfLoc).list()
-    if irisConfList != null then
+  if irisConfList != null then
     irisConfList.toList
-    else List()
+  else List()
 
 def linesOfAllConfigs() = 
   for (filename <- listOfConfigs()) do 
