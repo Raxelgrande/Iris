44132e936b8451e23981c009570dbdf6f9e037fb
Space Banana <tomasbessa@protonmail.com>
2024-03-13 00:29:22 +0000

    Further work on config reading

--- src/config/config.scala
+++ src/config/config.scala
@@ -6,31 +6,50 @@ import iris.distroFinder._
 import iris.tui._
 
 
-def listOfConfigs(): List[String] = 
+def listOfConfigs(): Array[String] = //I changed to array to remove the list conversion overhead
   val irisConfLoc = getHome()+"/.config/Iris/"
-  
   val irisConfList = File(irisConfLoc).list()
-  if irisConfList != null then
-    irisConfList.toList
-  else List()
 
-def linesOfAllConfigs(): String = //temporary code to show how its working
+  if irisConfList != null then
+    irisConfList
+  else Array()
+
+def linesOfAllConfigs(): String =
   var allLines = ""
   for filename <- listOfConfigs() do
-    val lines = 
-      Source.fromFile(getHome()+"/.config/Iris/"+filename)
-        .getLines()
-        .toVector //fuck it
-        .map(x => x + '\n')
-        .mkString //without parentheses works???? looks like its a different method
-    allLines += lines //procedural solution, alternative could be a recursive function
+    allLines += readConfig_string(filename)
   allLines
 
-def selectConfiguration(config: String): String = //maybe let the user select what config to read?
-  if config != "" then
-    config
-  else 
-    readLoop_getListString(listOfConfigs())
+def readConfig(filename: String): Iterator[String] =
+  Source.fromFile(getHome()+"/.config/Iris/"+filename).getLines()
+
+def readConfig_vector(filename: String): Vector[String] = readConfig(filename).toVector
+
+def readConfig_list(filename: String): List[String] = readConfig(filename).toList
+
+def readConfig_array(filename: String): Array[String] = readConfig(filename).toArray
+
+def readConfig_string(filename: String): String =
+  readConfig(filename)
+    .map(x => x + '\n')
+    .mkString
+
+def readAllConfigs(): Vector[String] = //1 whole string per config to avoid matrices
+  def groupConfigs(files: Array[String], cfgList: Vector[String] = Vector(), i: Int = 0): Vector[String] =
+    if i >= files.length then
+      cfgList
+    else
+      groupConfigs(files, cfgList :+ readConfig_string(files(i)), i+1)
+
+  val files = listOfConfigs()
+  groupConfigs(files)
+  
+//outdated, will have to rewrite some tui functions first
+// def selectConfiguration(config: String): String =
+//   if config != "" then
+//     config
+//   else 
+//     readLoop_getListString(listOfConfigs())
 
   
 
