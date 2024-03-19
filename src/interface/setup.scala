8cfdcae04bdddbf257e93fe5f517d3fbe01b1090
Raxelgrande <lucasvbm2@gmail.com>
2024-03-19 14:52:02 -0500

    change of plans

--- src/interface/setup.scala
+++ src/interface/setup.scala
@@ -1,11 +1,11 @@
-package iris.firstRun
+package iris.setup
 import iris.tui._
 import iris.config._
 import iris.distroFinder._
 import iris.sysUpdate.sysUpdate
 import iris.theming._
 
-def firstRun() =
+def setup() =
   getPackageManager()
   if askPrompt("We need to update your system to install all of the required dependencies that Iris needs to work properly." +
   "\nYou can still continue setting Iris up without updating, but you risk doing a partial update that can break your system" +
