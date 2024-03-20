46c8fe2e1aaffdb2cbd1de8eab1e4e73682b4d15
Space Banana <tomasbessa@protonmail.com>
2024-03-20 22:51:23 +0000

    Main Menu improvements and other stuff

--- iris/interface/interface.scala
+++ iris/interface/interface.scala
@@ -4,16 +4,20 @@ import iris.config._
 import iris.theming._
 
 def mainMenu() =
+  def create(existingConfs: Array[String]): String =
+    val confname = spawnAndRead("Please, write a name for your new configuration.")
+    if existingConfs.contains(confname) then
+      pressToContinue(s"The name $confname already exists")
+      create(existingConfs)
+    else
+      confname
+
   val home = chooseOption_string(Seq("Create a Configuration", "Load a Configuration", "Modify Configurations"), "Welcome to Iris!\nPlease select an option:")
-  if home == "Create a Configuration" then
-    def create() = spawnAndRead("Please, write a name for your new configuration.")
-    while listOfConfigs().contains(create()) do
-      pressToContinue("The name "+create()+" Already exists")
-      
-      
-  if home == "Load a Configuration" then 
-    val conflist = chooseOption_array(listOfConfigs(), "Select one of your configurations to load:")
-  else if home == "Modify Configurations" then 
-    val confchange = chooseOption_array(listOfConfigs(), "Select one of your configurations to modify:")
 
-
+  home match
+    case "Create a Configuration" =>
+      create(listOfConfigs())
+    case "Load a Configuration" =>
+      val conflist = chooseOption_array(listOfConfigs(), "Select one of your configurations to load:")
+    case "Modify Configurations" =>
+      val confchange = chooseOption_array(listOfConfigs(), "Select one of your configurations to modify:")
