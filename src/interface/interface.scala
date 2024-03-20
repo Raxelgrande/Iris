ec3f3da34e0a818c8697c0bedae8c1e15b618230
Raxelgrande <lucasvbm2@gmail.com>
2024-03-20 00:05:25 -0500

    Java stinks

--- src/interface/interface.scala
+++ src/interface/interface.scala
@@ -5,4 +5,10 @@ import iris.theming._
 import iris.setup._ 
 
 def mainMenu() =
-  val main = chooseOption_string(Seq("Load Configuration", "Modify Configurations", "Re-run setup"), "Welcome to Iris!\nPlease select an option:")
+  val home = chooseOption_string(Seq("Load a Configuration", "Modify Configurations", "Re-run Iris' Setup"), "Welcome to Iris!\nPlease select an option:")
+  if home == "Load a Configuration" then 
+    val conflist = chooseOption_array(listOfConfigs(), "Select one of your configurations to load:")
+  else if home == "Modify Configurations" then 
+    val confchange = chooseOption_array(listOfConfigs(), "Select one of your configurations to modify:")
+  else if home == "Re-run Iris' Setup" then 
+    setup()
--- src/main.scala
+++ src/main.scala
@@ -7,10 +7,13 @@ import iris.theming._
 import iris.themeSelector._
 import iris.config._
 import iris.tui.*
+import iris.distroFinder.getHome
 
 
 
 @main def main() =
-  mainMenu()
-  //replaceLine("testing", "gtktheme=", "vimix-dark-ruby")
+  // CRITICAL TO FIX! Java stinks and it changes the home location when running on root, possibly breaking tons of stuff.
+  println(System.getProperty("sun.java.command"))
+  println(getHome())
+  
   //if you read this you are a certified scalamancer
