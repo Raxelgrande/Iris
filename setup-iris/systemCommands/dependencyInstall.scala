0b535ec85ed24dae7e7b82a71f363a15767c4634
Raxelgrande <93939943+Raxelgrande@users.noreply.github.com>
2024-03-26 16:51:20 -0500

    Delete NixOS

---
+++ setup-iris/systemCommands/dependencyInstall.scala
@@ -0,0 +1,80 @@
+package setup.dependencyInstall
+import setup.sysUpdate._
+import setup.distroFinder.{getHome, getPackageManager}
+import scala.io.Source
+import scala.sys.process._
+import java.io.File
+import java.io.FileOutputStream
+
+
+def pacDependency() = List("pacman", "-Sy", "qt5ct", "qt6ct", "kvantum", "kvantum-qt5", "--noconfirm").!< 
+
+def dnfDependency() = List("dnf", "install", "qt5ct", "qt6ct", "kvantum", "kvantum-qt6", "-y").!<
+
+def zypDependency() = List("zypper", "install", "qt5ct", "qt6ct", "kvantum-manager", "kvantum-qt5", "kvantum-qt6").!< 
+
+def kvantumUbuntu() =
+  List("add-apt-repository", "ppa:papirus/papirus", "-y").!<
+  List("apt", "install", "qt5ct", "qt6ct", "qt5-style-kvantum", "qt6-style-kvantum", "-y").!<
+  aptUpdate()
+
+
+def kvantumDebian() =
+  writePapirusPPA()
+  List("wget", "-qO", "/etc/apt/trusted.gpg.d/papirus-ppa.asc", "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x9461999446FAF0DF770BFC9AE58A9D36647CAE7F").!<
+  aptUpdate()
+  List("apt", "install", "qt5ct", "qt6ct", "qt5-style-kvantum", "qt6-style-kvantum", "-y").!<
+
+
+def writePapirusPPA() =
+  val repo = "\ndeb http://ppa.launchpad.net/papirus/papirus/ubuntu jammy main".getBytes()
+  val repolist = FileOutputStream("/etc/apt/sources.list.d/papirus-ppa.list", true)
+  repolist.write(repo)
+  repolist.close()
+
+def etcEnvironment = FileOutputStream("/etc/environment", true)
+
+def writeQtPlatform() =
+  val qtPlatform = "QT_QPA_PLATFORMTHEME=qt5ct".getBytes()
+  etcEnvironment.write(qtPlatform)
+  etcEnvironment.close()
+  
+
+def writeQtForceX11() = //for advanced settings, in cases were wayland QT has issues
+  val qtX11 = "QT_QPA_PLATFORM=xcb".getBytes()
+  etcEnvironment.write(qtX11)
+  etcEnvironment.close()
+
+def flatpakGtkOverride() = List("flatpak", "override", "--filesystem="+getHome()+"/.themes").!<
+def flatpakIconOverride() = List("flatpak", "override", "--filesystem="+getHome()+"/.icons").!<
+
+//if flatpak isnt installed, the program will crash
+//try catch prevents this and also lets us know if flatpak is installed in the system
+//if flatpak is installed, we dont need to go through the native package manager to get it
+def kvantumFlatpak(): Boolean =
+  try
+    List("flatpak", "remote-add", "--if-not-exists", "flathub", "https://dl.flathub.org/repo/flathub.flatpakrepo").!<
+    List("flatpak", "install", "runtime/org.kde.KStyle.Kvantum/x86_64/6.6", "-y").!<
+    true
+    //note for later: even if flatpak is installed, the command can fail
+    //if that happens, this command execution will return an int that is not equal to 0
+  catch case e: Exception => false
+
+def kvantumOverride() = List("flatpak", "override", "--filesystem=xdg-config/Kvantum:ro").!<
+
+def pacmanFlatpak() = 
+  List("pacman", "-S", "flatpak", "--noconfirm").!<
+  kvantumFlatpak()
+
+def aptFlatpak() =
+  List("apt", "install", "flatpak", "-y").!<
+  kvantumFlatpak()
+
+def dnfFlatpak() =
+  List("dnf", "install", "flatpak", "-y").!<
+  kvantumFlatpak()
+
+def zypperFlatpak() = 
+  List("zypper", "install", "flatpak", "-y").!<
+  kvantumFlatpak()
+
---
+++ setup-iris/systemCommands/systemUpdate.scala
@@ -0,0 +1,67 @@
+package setup.sysUpdate
+
+import setup.distroFinder.getPackageManager
+import setup.dependencyInstall._
+import bananatui.*
+import scala.sys.process._
+import setup.distroFinder.aptWho
+
+
+val package_manager = getPackageManager()
+
+def sysUpdate() =
+  if package_manager != "nix" then //nix doesnt need to update
+    package_manager match
+      case "pacman" => pacUpdate()
+      case "apt" => aptUpdate()
+      case "dnf" => dnfUpdate()
+      case "zypper" => zypperUpdate()
+      case _ => unknownSystem()
+
+def sysDependencies() =
+  package_manager match
+    case "pacman" => pacDependency()
+    case "apt" => aptWho()
+    case "dnf" => dnfDependency()
+    case "zypper" => zypDependency()
+    case _ => unknownSystem("Dependencies. Check documentation at TODO DOC SITE")
+  
+
+def installKvantumFlatpak() =
+  if kvantumFlatpak() == false then //trying to run flatpak first, in case its already installed
+    package_manager match
+      case "pacman" => pacmanFlatpak()
+      case "apt" => aptFlatpak()
+      case "dnf" => dnfFlatpak()
+      case "zypper" => zypperFlatpak()
+      case _ => unknownSystem("Flatpak")
+
+
+def pacUpdate() = 
+  clear()
+  List("pacman", "-Syu", "--noconfirm").!<
+    
+def aptUpdate() = 
+  clear()
+  List("apt", "update", "-y").!<
+  List("apt", "upgrade", "-y").!<
+
+def dnfUpdate() = 
+  clear()
+  List("dnf", "upgrade", "-y").!<
+
+def zypperUpdate() = 
+  clear()
+  List("zypper", "dup", "-y").!<
+
+
+def unknownSystem(pkg_name: String = "") =
+  val txt =
+    if pkg_name != "" then
+      "Iris could not recognise your system or the package manager it uses"
+      + s"\nPlease update your system before installing $pkg_name"
+    else
+      "Iris could not recognise your system or the package manager it uses"
+      + "\nPlease update your system before installing the required packages"
+  pressToContinue(txt)
+
