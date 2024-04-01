7934bd6b1c24f6cf0066ea6bf5e9786ed682343e
Raxelgrande <lucasvbm2@gmail.com>
2024-04-01 17:56:41 -0500

    Remove Native sh

--- native.sh
+++
@@ -1,7 +0,0 @@
-echo "Building Linux native binaries (glibc, static, x86_64)"
-native-image --no-fallback --static -O3 -jar iris-java.jar -o Iris
-native-image --no-fallback --static -O3 -jar setup-iris-java.jar -o SetupIris
-native-image --no-fallback --static -03 -jar misc-flatpak-override-java.jar -o MiscFlatpakOverride
-
-echo "Packaging Linux native binaries"
-7z a -mx7 -mmt0 iris-linux-x86_64.zip Iris SetupIris MiscFlatpakOverride
