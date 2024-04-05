54fe9848cbdc92cc6884ea50b2572c447700dba6
Raxelgrande <lucasvbm2@gmail.com>
2024-04-05 18:19:55 -0500

    Gives TUI to misc but can't import the library????

--- misc/misc.scala
+++ misc/misc.scala
@@ -1,11 +1,17 @@
 package misc.misc 
 import scala.sys.process._
+import bananatui.*
+
 
 @main def main() =
   def getHome() = System.getProperty("user.home")
 
   def flatpakGtkOverride() = List("sudo", "flatpak", "override", "--filesystem="+getHome()+"/.themes").!<
   def flatpakIconOverride() = List("sudo", "flatpak", "override", "--filesystem="+getHome()+"/.icons").!<
-
-  flatpakIconOverride()
-  flatpakGtkOverride()
+  val finishFlatpakOverrides = askPrompt("Due to limitations of Iris and the JVM's libraries, we had to make this third program to finish Iris' setup" +
+    "\nIf you want to allow Iris to theme flatpaks' Icon and GTK themes say yes.")
+  if finishFlatpakOverrides == true then 
+    flatpakIconOverride()
+    flatpakGtkOverride()
+  else 
+    System.exit(0)
