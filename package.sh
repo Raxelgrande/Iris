107544961ae5bfcd35a0bae3c0a91744463691d9
Space Banana <tomasbessa@protonmail.com>
2024-04-06 23:17:20 +0100

    package.sh fixes and cleanup.sh

--- package.sh
+++ package.sh
@@ -6,9 +6,9 @@ dir_misc="misc/*.scala"
 echo "Building lightweight JAR"
 scalac $dir_iris $dir_lib -d iris.jar
 scalac $dir_setup $dir_lib -d setup-iris.jar
-scalac $dir_misc -d misc-flatpak-override.jar
+scalac $dir_misc $dir_lib -d misc-flatpak-override.jar
 
 echo "Building Fat JAR"
 scala-cli --power package iris lib --assembly --preamble=false --jvm 11 -f -o iris-java.jar
 scala-cli --power package setup-iris lib --assembly --preamble=false --jvm 11 -f -o setup-iris-java.jar
-scala-cli --power package misc --assembly --preamble=false --jvm 11 -f -o misc-flatpak-override-java.jar
+scala-cli --power package misc lib --assembly --preamble=false --jvm 11 -f -o misc-flatpak-override-java.jar
