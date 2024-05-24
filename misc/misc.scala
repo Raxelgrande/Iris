9ae27c356e02aece331bb7db12b020e0899f0e33
Lucas V <93939943+Raxelgrande@users.noreply.github.com>
2024-05-24 13:40:46 -0500

    Adds flatpakDotConfig for theme compatibility improvements

--- misc/misc.scala
+++ misc/misc.scala
@@ -7,11 +7,13 @@ import bananatui.*
   def getHome() = System.getProperty("user.home")
 
   def flatpakGtkOverride() = List("sudo", "flatpak", "override", "--filesystem="+getHome()+"/.themes").!<
+  def flatpakDotConfigOverride() = List("sudo", "flatpak", "override", "--filesystem="+getHome()+"/.config").!<
   def flatpakIconOverride() = List("sudo", "flatpak", "override", "--filesystem="+getHome()+"/.icons").!<
   val finishFlatpakOverrides = askPrompt("Due to limitations of Iris and the JVM's libraries, we had to make this third program to finish Iris' setup" +
-    "\nIf you want to allow Iris to theme flatpaks' Icon and GTK themes say yes.")
+    "\nIf you want to allow Iris to theme flatpaks' Icon, GTK themes and .config directory for compatibility reasons, say yes.")
   if finishFlatpakOverrides == true then 
     flatpakIconOverride()
     flatpakGtkOverride()
+    flatpakDotConfigOverride()
   else 
     System.exit(0)
